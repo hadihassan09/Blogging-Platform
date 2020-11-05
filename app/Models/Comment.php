c7cfa0bd082cb3206dad8ee48164750a19989ce2
Hadi Hassan <hadihassan09@gmail.com>
2020-11-05 13:47:34 -0800

    added getCreatorName for comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Comment extends Model
 {
     use HasFactory;
+    protected $guarded=[];
 
     public function post()
     {
         return $this->belongsTo('App\Models\Post');
+    }
+
+    public function getCreaterName(){
+        return User::find($this->user_id)->name;
     }
 }
