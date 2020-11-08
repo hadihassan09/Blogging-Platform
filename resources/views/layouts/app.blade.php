d7f8d826d41be516dc9cbf6800fd18a62a17066a
Hadi Hassan <hadihassan09@gmail.com>
2020-11-08 04:36:05 -0800

    refactored like button design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,12 +16,6 @@
         <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
               integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-
-        <style>
-            .like-post{
-                color: blue !important;
-            }
-        </style>
         @yield('header')
 
         @livewireStyles
